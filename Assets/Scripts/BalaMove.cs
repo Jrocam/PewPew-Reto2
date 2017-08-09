f3a82d1534c669bad957968aa08a73ce23d703de
Juan Roca <juanmaa13@gmail.com>
2017-08-09 13:24:28 -0500

    PewPew
    Disparitos con unity

---
+++ Assets/Scripts/BalaMove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalaMove : MonoBehaviour {
+    public float projectileSpeed = 10f;
+    //AUDIO
+    public AudioClip ded1;
+    public AudioClip ded2;
+    private AudioSource source;
+
+    // Use this for initialization
+    void Start () {
+        source = GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.forward * projectileSpeed * Time.deltaTime);
+        //GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0, 0, projectileSpeed));
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        float rand = Random.Range(1.0f, 2.0f);
+
+        if (rand > 1.5f)
+            source.PlayOneShot(ded2,rand);
+        else
+            source.PlayOneShot(ded1,rand);
+
+        Destroy(other.gameObject);
+        //Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/Disparar.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disparar : MonoBehaviour {
+    //public Rigidbody balaRigid;
+    public GameObject projectile;
+    //Audio
+    public AudioClip pewPew;
+    private AudioSource source;
+    private float volLowRange = .4f;
+    private float volHighRange = 1.2f;
+
+    // Use this for initialization
+    void Start() {
+        //balaRigid = GetComponent<Rigidbody>();
+        source = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        //transform.Translate(Vector3.forward * projectileSpeed * Time.deltaTime);
+        //balaRigid.velocity = new Vector3(Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime, 0f , Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime);
+        if (Input.GetButtonDown("Fire1")) {
+            float vol = Random.Range(volLowRange,volHighRange);
+            source.PlayOneShot(pewPew, vol);
+
+            GameObject throwThis = Instantiate(projectile,transform.position, transform.rotation);
+            //throwThis.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0,0,projectileSpeed));
+            // Destroy the bullet after 2 seconds
+            Destroy(throwThis, 2.0f);
+
+        }
+
+    }
+
+}
